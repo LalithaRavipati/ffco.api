8b8b592d7668bd56a94e773ce0730b198f364498
Richard Kutz <rkutz@hach.com>
2016-09-02 16:40:04 -0600

    FOART-626: Initial solution creation.

---
+++ Hach.Fusion.FFCO.Api/Hach.Fusion.FFCO.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Hach.Fusion.FFCO.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Hach.Fusion.FFCO.Api/Hach.Fusion.FFCO.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Hach.Fusion.FFCO.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
