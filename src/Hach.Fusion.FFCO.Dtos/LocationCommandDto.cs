f0eff8ed5f0df89a25353bd1ac56ec9719dbd951
Kent Miller <kmiller@hach.com>
2016-09-12 15:35:28 -0600

    Implemented Dto classes

---
+++ src/Hach.Fusion.FFCO.Dtos/LocationCommandDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hach.Fusion.FFCO.Dtos
+{
+    /// <summary>
+    /// Data Transfer Object (DTO) for location entities used with create and update controller commands.
+    /// </summary>
+    public class LocationCommandDto : LocationBaseDto
+    {
+        public Guid TenantId { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid LocationTypeId { get; set; }
+
+        public Guid? ParentId { get; set; }
+    }
+}
