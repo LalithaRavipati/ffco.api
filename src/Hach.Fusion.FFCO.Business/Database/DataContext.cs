db562f5bb7dbe7ddd88082143b8da0fa8f34e27f
Kent Miller <kmiller@hach.com>
2016-09-12 16:01:31 -0600

    Base implementation of Business

---
+++ src/Hach.Fusion.FFCO.Business/Database/DataContext.cs
@@ -0,0 +1,56 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using Hach.Fusion.FFCO.Business.Extensions;
+using Hach.Fusion.FFCO.Entities;
+
+namespace Hach.Fusion.FFCO.Business.Database
+{
+    /// <summary>
+    /// The database context for FFStorage.
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public DataContext()
+        { }
+
+        /// <summary>
+        /// Constructor that accepts a database connection string.
+        /// </summary>
+        /// <param name="connectionString">The database connection string.</param>
+        public DataContext(string connectionString) : base(connectionString)
+        {
+            // Read the DateTimeKindAttributes and set them during entity materialization when appopriate.
+            ((IObjectContextAdapter)this).ObjectContext.ObjectMaterialized +=
+                (sender, e) => DateTimeKindAttribute.Apply(e.Entity);
+        }
+
+        /// <summary>
+        /// Gets or sets the DbSet containing <see cref="Locations"/> entities.
+        /// </summary>
+        public DbSet<Location> Locations { get; set; }
+
+        /// <summary>
+        /// Gets or sets the DbSet containing <see cref="Parameters"/> entities.
+        /// </summary>
+        public DbSet<Parameter> Parameters { get; set; }
+
+        /// <summary>
+        /// Gets or sets the DbSet containing <see cref="ParameterType"/> entities.
+        /// </summary>
+        public DbSet<ParameterType> ParameterTypes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the DbSet containing <see cref="UnitType"/> entities.
+        /// </summary>
+        public DbSet<UnitType> UnitTypes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the DbSet containing <see cref="ChemicalFormTypes"/> entities.
+        /// </summary>
+        public DbSet<ChemicalFormType> ChemicalFormTypes { get; set; }
+    }
+}
---
+++ src/Hach.Fusion.FFCO.Business/Extensions/DateTimeKindAttribute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Hach.Fusion.FFCO.Business.Extensions
+{
+    /// <summary>
+    /// Allows you to specify the DateTimeKind to a DateTime property so that entity framework can set the right DateTimeKind automatically.
+    /// </summary>
+    /// <remarks>
+    /// This code was found at: http://www.gitshah.com/2015/02/how-to-automatically-set-datetimekind.html and http://stackoverflow.com/questions/4648540/entity-framework-datetime-and-utc
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DateTimeKindAttribute : Attribute
+    {
+        public DateTimeKindAttribute(DateTimeKind kind)
+        {
+            Kind = kind;
+        }
+
+        public DateTimeKind Kind { get; }
+
+        public static void Apply(object entity)
+        {
+            if (entity == null)
+                return;
+
+            //Find all properties that are of type DateTime or DateTime?;
+            var properties = entity.GetType().GetProperties()
+                .Where(x => x.PropertyType == typeof(DateTime)
+                         || x.PropertyType == typeof(DateTime?));
+
+            foreach (var property in properties)
+            {
+                //Check whether these properties have the DateTimeKindAttribute;
+                var attr = property.GetCustomAttribute<DateTimeKindAttribute>();
+                if (attr == null)
+                    continue;
+
+                var dt = property.PropertyType == typeof(DateTime?)
+                    ? (DateTime?)property.GetValue(entity)
+                    : (DateTime)property.GetValue(entity);
+
+                if (dt == null)
+                    continue;
+
+                //If the value is not null set the appropriate DateTimeKind;
+                property.SetValue(entity, DateTime.SpecifyKind(dt.Value, attr.Kind));
+            }
+        }
+    }
+}
