b5ba3430cbbbc09e80504eb3750263d41a047cf3
Kent Miller <kmiller@hach.com>
2016-09-13 09:34:26 -0600

    Completed scaffolding.

---
+++ src/Hach.Fusion.FFCO.Api/App_Start/CustomPropertyRoutingConvention.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Controllers;
+using System.Web.OData.Routing;
+using System.Web.OData.Routing.Conventions;
+
+namespace Hach.Fusion.FFCO.Api
+{
+    /// <summary>
+    /// Navigation routing convention to support property path segments in urls.
+    /// </summary>
+    /// <remarks>
+    /// Note that this version of the property routing convention handles the case
+    /// where the routing path template contains "unresolved" for the property name.
+    /// This can occur when the OData Entity Data Model uses property names that are derived
+    /// an entity set.
+    /// </remarks>
+    public class CustomPropertyRoutingConvention : NavigationSourceRoutingConvention
+    {
+        private const string ActionName = "GetProperty";
+
+        /// <inheritdoc />
+        public override string SelectAction(ODataPath odataPath, HttpControllerContext controllerContext,
+            ILookup<string, HttpActionDescriptor> actionMap)
+        {
+            if (odataPath == null || controllerContext == null || actionMap == null)
+            {
+                return null;
+            }
+
+            if (odataPath.PathTemplate != "~/entityset/key/property" &&
+                odataPath.PathTemplate != "~/entityset/key/unresolved" &&
+                odataPath.PathTemplate != "~/entityset/key/cast/property" &&
+                odataPath.PathTemplate != "~/singleton/property" &&
+                odataPath.PathTemplate != "~/singleton/cast/property")
+            {
+                return null;
+            }
+
+            var propertyName = GetPropertyName(odataPath.Segments);
+
+            if (propertyName == null)
+                return null;
+
+            var actionName = FindMatchingAction(actionMap, ActionName);
+
+            if (actionName == null)
+            {
+                return null;
+            }
+
+            if (odataPath.PathTemplate.StartsWith("~/entityset/key", StringComparison.Ordinal))
+            {
+                var keyValueSegment = odataPath.Segments[1] as KeyValuePathSegment;
+                // ReSharper disable once PossibleNullReferenceException
+                controllerContext.RouteData.Values[ODataRouteConstants.Key] = keyValueSegment.Value;
+            }
+
+            controllerContext.RouteData.Values["propertyName"] = propertyName;
+
+            return actionName;
+        }
+
+        /// <summary>
+        /// Retrieves the property name from the routing segments.
+        /// </summary>
+        /// <param name="segments">Routing segments resulting from a parsed request.</param>
+        /// <returns>The property name from the routing segments or null if the property name is not available.</returns>
+        private static string GetPropertyName(IList<ODataPathSegment> segments)
+        {
+            var propertySegment = segments[segments.Count - 1] as PropertyAccessPathSegment;
+
+            if (propertySegment == null)
+            {
+                var unresolvedSegment = segments[segments.Count - 1] as UnresolvedPathSegment;
+                return unresolvedSegment?.SegmentValue;
+            }
+
+            return propertySegment.PropertyName;
+        }
+
+        /// <summary>
+        /// Finds a controller action.
+        /// </summary>
+        /// <param name="actionMap">Map containing all actions.</param>
+        /// <param name="targetActionName">Name of the action to find.</param>
+        /// <returns>Name of action.</returns>
+        public static string FindMatchingAction(ILookup<string, HttpActionDescriptor> actionMap,
+            string targetActionName)
+        {
+            return actionMap.Contains(targetActionName) ? targetActionName : null;
+        }
+    }
+}
---
+++ src/Hach.Fusion.FFCO.Api/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System.Configuration;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Hach.Fusion.Core.Api.Controllers;
+using Hach.Fusion.Core.Api.Security;
+using Hach.Fusion.FFCO.Api.AutofacModules;
+using Hach.Fusion.FFCO.Business;
+using IdentityServer3.AccessTokenValidation;
+using Microsoft.Owin;
+using Owin;
+using Thinktecture.IdentityModel;
+
+// Flags this class as needing to be called by OWIN at startup.
+[assembly: OwinStartup(typeof(Hach.Fusion.FFCO.Api.Startup))]
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace Hach.Fusion.FFCO.Api
+{
+    /// <summary>
+    /// Code executed by OWIN at application startup.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Configures the Web API services offered by this project.
+        /// </summary>
+        /// <param name="app">OWIN application builder that provides web services.</param>
+        public void Configuration(IAppBuilder app)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var controllerSelector = new ODataVersionControllerSelector(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configuration.Services.Replace(typeof(IHttpControllerSelector), controllerSelector);
+
+            MappingManager.Initialize();
+
+            var config = GlobalConfiguration.Configuration;
+
+            // Configure Autofac
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<ServiceModule>();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            var container = builder.Build();
+
+            app.UseAutofacMiddleware(container);
+
+            // Dependency for Web API
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            var authorityUrl = ConfigurationManager.AppSettings["IdServerUri"];
+            app.UseIdentityServerBearerTokenAuthentication(
+                new IdentityServerBearerTokenAuthenticationOptions
+                {
+                    Authority = authorityUrl,
+                    RequiredScopes = new[] {"FFAccessAPI"}
+                });
+
+            app.ConfigureAuth();
+
+            ClaimsAuthorization.CustomAuthorizationManager = new AuthorizationManager();
+
+            var scope = container.BeginLifetimeScope();
+            var service = scope.Resolve<ClaimsTransformer>();
+            app.UseClaimsTransformation(service);
+        }
+    }
+}
