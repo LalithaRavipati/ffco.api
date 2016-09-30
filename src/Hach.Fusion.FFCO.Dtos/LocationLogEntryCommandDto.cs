da5e8349bd91422ea64146c7fa2549fee1f5a37a
Richard Kutz <rkutz@hach.com>
2016-09-29 19:17:27 -0600

    FOART-643: Added LocationLogEntry controllers, DTOs, and docs. All is pretty rough. Updated NuGet for entities. Added Hach icon to home page.

---
+++ src/Hach.Fusion.FFCO.Dtos/LocationLogEntryCommandDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hach.Fusion.FFCO.Dtos
+{
+    /// <summary>
+    /// Data Transfer Object (DTO) for Location Log Entry entities used with create and update controller commands.
+    /// </summary>
+    /// <remarks>
+    /// This class doesn't provide state or actions. It is needed in order for OData
+    /// to function properly. OData controllers must be associated with one and only one entity
+    /// type. However, different entity types are allowed if they derive from that one entity.
+    /// </remarks>
+    public class LocationLogEntryCommandDto : LocationBaseDto
+    {
+    }
+}
---
+++ src/Hach.Fusion.FFCO.Dtos/LocationLogEntryQueryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hach.Fusion.FFCO.Dtos
+{
+    /// <summary>
+    /// Data Transfer Object (DTO) for Location Log Entry entities used with query controller commands.
+    /// </summary>
+    public class LocationLogEntryQueryDto : LocationLogEntryBaseDto
+    {
+        public Guid CreatedById { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Guid ModifiedById { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
