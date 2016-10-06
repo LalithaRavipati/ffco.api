f316886ccaa34f913a92fe717b557b87eaac742a
Kent Miller <kmiller@hach.com>
2016-10-06 08:25:54 -0600

    FOART-1035: Updated nuget packages. Created LimitType Dto classes.

---
+++ src/Hach.Fusion.FFCO.Dtos/LimitTypes/LimitTypeCommandDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Hach.Fusion.FFCO.Dtos.LimitTypes
+{
+    /// <summary>
+    /// Data Transfer Object (DTO) for LimitType entities used with create and update controller commands.
+    /// </summary>
+    public class LimitTypeCommandDto : LimitTypeBaseDto
+    {
+        public string I18NKeyName { get; set; }
+        public int Severity { get; set; }
+        public int Polarity { get; set; }
+    }
+}
