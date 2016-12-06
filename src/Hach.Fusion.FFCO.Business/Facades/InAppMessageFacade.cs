92aab8cc17e4253bb0eff6c48c65bfe049543a2a
Aaron Krone <akrone@hach.com>
2016-12-06 15:56:42 -0700

    FOART-1244 Fixing a couple of Validator typos in the XML Documentation
    FOART-1244 Checking in API changes incase the cold gets me before I come back
    
    FOART-1244 Added TODO comment

---
+++ src/Hach.Fusion.FFCO.Business/Facades/InAppMessageFacade.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.OData;
+using System.Web.OData.Query;
+using AutoMapper;
+using Hach.Fusion.Core.Api.Security;
+using Hach.Fusion.Core.Business.Facades;
+using Hach.Fusion.Core.Business.Results;
+using Hach.Fusion.Core.Business.Validation;
+using Hach.Fusion.FFCO.Business.Database;
+using Hach.Fusion.FFCO.Business.Extensions;
+using Hach.Fusion.FFCO.Core.Dtos;
+using Hach.Fusion.FFCO.Core.Entities;
+using System.Threading;
+
+namespace Hach.Fusion.FFCO.Business.Facades
+{
+    /// <summary>
+    /// Facade for managing the InAppMessage repository. 
+    /// </summary>    
+    public class InAppMessageFacade
+        : FacadeWithCruModelsBase<InAppMessageCommandDto, InAppMessageCommandDto, InAppMessageQueryDto, Guid>
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Constructor for the <see cref="InAppMessageFacade"/> class taking a database context
+        /// and validator argument.
+        /// </summary>
+        /// <param name="context">Database context containing InAppMessage entities.</param>
+        /// <param name="validator">Validator for InAppMessage DTOs.</param>
+        public InAppMessageFacade(DataContext context, IFFValidator<InAppMessageCommandDto> validator)
+        {
+            _context = context;
+
+            ValidatorCreate = validator;
+            ValidatorUpdate = validator;
+
+            _mapper = MappingManager.AutoMapper;
+        }
+
+        #region Get Methods
+
+        /// <summary>
+        /// Gets a list of InAppMessages from the data store.
+        /// </summary>
+        /// <param name="queryOptions">OData query options.</param>
+        /// <returns>
+        /// An asynchronous task result containing information needed to create an API response message.
+        /// If successful, the task result contains the list of InAppMessage DTOs retrieved.
+        /// </returns>
+        public override async Task<QueryResult<InAppMessageQueryDto>> Get(ODataQueryOptions<InAppMessageQueryDto> queryOptions)
+        {
+            queryOptions.Validate(ValidationSettings);
+
+            var uid = GetCurrentUser();
+
+            if (!uid.HasValue)
+                return Query.Error(GeneralErrorCodes.TokenInvalid("UserId"));
+
+            var results = await Task.Run(() => _context.GetInAppMessagesForUser(uid.Value)
+                .Select(_mapper.Map<InAppMessage, InAppMessageQueryDto>)
+                .AsQueryable())
+                .ConfigureAwait(false);
+
+            return Query.Result(results);
+        }
+
+        /// <summary>
+        /// Gets a single InAppMessage from the data store.
+        /// </summary>
+        /// <param name="id">ID that uniquely identifies the InAppMessage to be retrieved.</param>
+        /// <returns>
+        /// An asynchronous task result containing information needed to create an API response message.
+        /// If successful, the task result includes the InAppMessage DTO retrieved.
+        /// </returns>
+        public override async Task<QueryResult<InAppMessageQueryDto>> Get(Guid id)
+        {
+            var uid = GetCurrentUser();
+
+            if (!uid.HasValue)
+                return Query.Error(GeneralErrorCodes.TokenInvalid("UserId"));
+
+            var result = await Task.Run(() => _context.GetInAppMessagesForUser(uid.Value)
+                .FirstOrDefault(msg => msg.Id == id))
+                .ConfigureAwait(false);
+
+            if (result == null)
+                return Query.Error(EntityErrorCode.EntityNotFound);
+
+            var dto = _mapper.Map<InAppMessage, InAppMessageQueryDto>(result);
+
+            return Query.Result(dto);
+        }
+
+        private Guid? GetCurrentUser()
+        {
+            var userId = Thread.CurrentPrincipal == null ? null : Thread.CurrentPrincipal.GetUserIdFromPrincipal();
+
+            if (userId == null)
+                return null;
+            else
+                return Guid.Parse(userId);
+        }
+
+        #endregion Get Methods
+
+        #region Update Methods
+
+        public override Task<CommandResult<InAppMessageCommandDto, Guid>> Update(Guid id, Delta<InAppMessageCommandDto> delta)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Not Implemented Methods
+
+        public override Task<QueryResult<InAppMessageQueryDto>> GetProperty(Guid id, string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<CommandResult<InAppMessageQueryDto, Guid>> Create(InAppMessageCommandDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<CommandResult<InAppMessageQueryDto, Guid>> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override Task<CommandResult<InAppMessageQueryDto, Guid>> CreateReference(Guid id, string navigationProperty, object referenceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<CommandResult<InAppMessageQueryDto, Guid>> DeleteReference(Guid id, string navigationProperty, object referenceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Not Implemented Methods
+    }
+}
--- src/Hach.Fusion.FFCO.Business/Validators/InAppMessageValidator.cs
+++ src/Hach.Fusion.FFCO.Business/Validators/InAppMessageValidator.cs
@@ -1,36 +1,32 @@
 ﻿using System;
 using Hach.Fusion.Core.Business.Validation;
 using Hach.Fusion.FFCO.Core.Dtos;
+using Hach.Fusion.FFCO.Core.Entities;
 
 namespace Hach.Fusion.FFCO.Business.Validators
 {
     /// <summary>
-    /// Validates <see cref="LocationTypeCommandDto"/>s.
+    /// Validates <see cref="InAppMessageCommandDto"/>s.
     /// </summary>
-    public class LocationTypeValidator : FFValidator<LocationTypeCommandDto, Guid>
+    /// <remarks>
+    /// </remarks>
+    public class InAppMessageValidator : FFValidator<InAppMessageCommandDto, Guid>
     {
-        private const int NameMaximumLength = 100;
-
-        private const int NameMinimumLength = 4;
-
         /// <summary>
-        /// Validates the state of the specified <see cref="LocationTypeCommandDto"/>.
+        /// Validates the state of the specified <see cref="InAppMessageCommandDto"/>.
         /// </summary>
         /// <param name="dto">Data transfer object whose state is to be validated.</param>
-        public override FFValidationResponse Validate(LocationTypeCommandDto dto)
+        public override FFValidationResponse Validate(InAppMessageCommandDto dto)
         {
             IsNull(dto);
+
+            IsType(dto, typeof(InAppMessageCommandDto));
 
             if (FFErrors.Count > 0)
                 return new FFValidationResponse
                 {
                     FFErrors = FFErrors
                 };
-
-            Evaluate(l => l.I18NKeyName, dto.I18NKeyName)
-                .Required()
-                .MaxLength(NameMaximumLength)
-                .MinLength(NameMinimumLength);         
 
             return new FFValidationResponse
             {
