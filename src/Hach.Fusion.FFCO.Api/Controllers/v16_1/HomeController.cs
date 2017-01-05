1092118d8de8676800eb6853c148e4f6cd164ec7
Aaron Krone <akrone@hach.com>
2017-01-05 14:45:32 -0700

    FOART-1338 Reverting web.config IdSvr url setting. Bumping API assembly Versions

---
+++ src/Hach.Fusion.FFCO.Api/Controllers/v16_1/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace Hach.Fusion.FFCO.Api.Controllers
+{
+    /// <summary>
+    /// Home page controller for API.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
