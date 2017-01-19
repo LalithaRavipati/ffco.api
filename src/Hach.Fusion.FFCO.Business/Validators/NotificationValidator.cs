49424daf814d1339d40b4bce6fceff6f6a45a56b
Aaron Krone <akrone@hach.com>
2017-01-19 15:47:57 -0700

    FOART-1340 Updating Swagger Docs to list the current uploadTransactionTypes

--- src/Hach.Fusion.FFCO.Business/Validators/NotificationValidator.cs
+++ src/Hach.Fusion.FFCO.Business/Validators/NotificationValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Hach.Fusion.Core.Business.Validation;
 using Hach.Fusion.FFCO.Core.Dtos;
+using System;
 
 namespace Hach.Fusion.FFCO.Business.Validators
 {
@@ -8,7 +9,7 @@ namespace Hach.Fusion.FFCO.Business.Validators
     /// </summary>
     /// <remarks>
     /// </remarks>
-    public class NotificationValidator : FFValidator<NotificationDto>
+    public class NotificationValidator : FFValidator<NotificationDto, Guid>
     {
         /// <summary>
         /// Validates the state of the specified <see cref="NotificationDto"/>.
