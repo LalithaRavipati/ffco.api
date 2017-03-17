5ce8a70893fe922c8a3652a51fc0b81a961321ea
Richard Kutz <rkutz@hach.com>
2017-03-17 15:56:04 -0600

    FOART-2216: Added Files controller and facade.

---
+++ src/Hach.Fusion.FFCO.Api/Controllers/v16_1/FilesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.OData;
+using System.Web.OData.Query;
+using Hach.Fusion.Core.Api.Controllers;
+using Hach.Fusion.Core.Api.OData;
+using Hach.Fusion.Core.Api.Security;
+using Hach.Fusion.Core.Business.Facades;
+using Hach.Fusion.Core.Enums;
+
+using Swashbuckle.Swagger.Annotations;
+using Hach.Fusion.Data.Dtos;
+
+namespace Hach.Fusion.FFCO.Api.Controllers.v16_1
+{
+    /// <summary>
+    /// Web API controller for managing blob storage files whose metadata is stored in DocumentDb.
+    /// </summary>
+    /// <remarks>
+    /// All of the public methods below return an asynchronous task result containing information needed to create
+    /// an API response message. Client applications using this API will not receive the task, but will instead receive
+    /// a response message that originates from information in the task result. Since clients using this API, will
+    /// see the XML comments in this class, the "return" fields below indicate the information returned to the
+    /// client applications.
+    /// </remarks>
+    public class FilesController : FFAABaseController<ParameterQueryDto, Guid>
+    {
+        /// <summary>
+        /// Default constructor for the <see cref="ParametersController"/> class taking OData helper and repository facade arguments.
+        /// </summary>
+        /// <param name="oDataHelper">Helper that provides OData utilities to manage requests.</param>
+        /// <param name="facade">Facade for the repository used to retrieve location type data.</param>
+        public FilesController(IODataHelper oDataHelper, IFacade<ParameterQueryDto, Guid> facade) 
+            : base(oDataHelper)
+        {
+            if (facade == null)
+                throw new ArgumentNullException(nameof(facade));
+
+            _facade = facade;
+        }
+
+        /// <summary>
+        /// Retrieves the Parameter with the specified ID.
+        /// </summary>
+        /// <param name="key">ID that identifies the entity to be retrieved.</param>
+        /// <param name="queryOptions">OData query options.</param>
+        /// <returns>
+        /// The DTO for the indicated Parameter.
+        /// </returns>
+        /// <example>
+        /// GET: ~/odata/v16.1/Parameters(CDB928DA-365A-431E-A419-E9D6AF0C4FE5)
+        /// </example>
+        /// <include file='XmlDocumentation/ParametersController.doc' path='ParametersController/Methods[@name="GetOne"]/*'/>
+        [FFSEAuthorize(PermissionAction.Read)]
+        [EnableQuery(MaxExpansionDepth=Constants.DefaultMaxExpansionDepth)]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        [SwaggerResponse(HttpStatusCode.InternalServerError)]
+        [ResponseType(typeof(ParameterQueryDto))]
+        public async Task<IHttpActionResult> Get([FromODataUri] Guid key, ODataQueryOptions<ParameterQueryDto> queryOptions)
+        {
+            var results = await _facade.Get(key);
+            return Query(results);
+        }
+    }
+}
